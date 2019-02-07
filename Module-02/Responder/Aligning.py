8ba5e411704976f8830312978ec0c8c9f6953903
RohitGupta <rohit0611gupta@gmail.com>
2019-02-07 22:37:13 +0530

    Updated Module 2

---
+++ Module-02/Responder/Aligning.py
@@ -0,0 +1,9 @@
+# Aligning Strings
+
+Alphastring = "|{:<15}|{:^15}|{:>15}|".format('Hey', 'Whatsupp', 'Buddy!')
+print("Formatted String is ",Alphastring)
+
+# Aligning Numbers
+
+NumericString = "|{0:<15d}|{0:^15d}|{0:>15d}|".format(125,125,125)
+print("Formatted String is ",NumericString)
